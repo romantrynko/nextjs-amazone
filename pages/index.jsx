a6435c6f9d4a1d8af19b2125b78db617975c05d6
Roman Trynko <trynkoroman25@gmail.com>
2022-10-14 11:11:40 +0300

    handled getting products from db

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,13 @@
 import Layout from '../components/Layout';
 import ProductItem from '../components/ProductItem';
-import { data } from '../utils/data';
+import Product from '../models/Product';
+import db from '../utils/db';
 
-export default function Home() {
+export default function Home({ products }) {
   return (
     <Layout title={'Home Page'}>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
-        {data.products.map((product) => (
+        {products.map((product) => (
           <ProductItem
             key={product.slug}
             product={product}
@@ -16,3 +17,13 @@ export default function Home() {
     </Layout>
   );
 }
+
+export const getServerSideProps = async () => {
+  await db.connect();
+  const products = await Product.find().lean();
+  return {
+    props: {
+      products: products.map(db.convertDocToObj)y
+    }
+  };
+};
