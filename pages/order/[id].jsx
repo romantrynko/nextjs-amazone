0230db8fbc0c71b9540323fa2eb5b8de41e36855
Roman Trynko <trynkoroman25@gmail.com>
2022-10-17 13:25:56 +0300

    added sections to order page

--- pages/order/[id].jsx
+++ pages/order/[id].jsx
@@ -1,8 +1,10 @@
-import { useRouter } from 'next/router';
-import { useEffect, useReducer } from 'react';
+import Image from 'next/image';
 import Layout from '../../components/Layout';
+import Link from 'next/link';
 import axios from 'axios';
 import { getError } from '../../utils/error';
+import { useEffect, useReducer } from 'react';
+import { useRouter } from 'next/router';
 
 function reducer(state, action) {
   switch (action.type) {
@@ -36,8 +38,8 @@ function OrderScreen() {
         dispatch({ type: 'FETCH_REQUEST' });
         const { data } = await axios.get(`/api/orders/${orderId}`);
         dispatch({ type: 'FETCH_SUCCESS', payload: data });
-      } catch (error) {
-        dispatch({ type: 'FETCH_FAIL', payload: getError(error) });
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: getError(err) });
       }
     };
 
@@ -46,15 +48,91 @@ function OrderScreen() {
     }
   }, [order, orderId]);
 
+  const {
+    shippingAddress,
+    paymentMethod,
+    orderItems,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+    isPaid,
+    paidAt,
+    isDelivered,
+    deliveredAt
+  } = order;
+
   return (
-    <Layout title='Your order'>
+    <Layout title="Your order">
       <h1 className="mb-4 text-xl">{`Order #${orderId}`}</h1>
       {loading ? (
         <div>Loading...</div>
       ) : error ? (
-        <div className="alert-error"> </div>
+        <div className="alert-error">{error}</div>
       ) : (
-        <div className="grid md:grid-cols-4 md:gap-5"></div>
+        <div className="grid md:grid-cols-4 md:gap-5">
+          <div className="overflow-x-auto md:col-span-3">
+            <div className="card p-5">
+              <h2 className="mb-2 text-lg">Shipping Address</h2>
+              <div className="mb-2">
+                {shippingAddress.fullName}, {shippingAddress.address},{' '}
+                {shippingAddress.city}, {shippingAddress.postalCode},{' '}
+                {shippingAddress.country}
+              </div>
+              {isDelivered ? (
+                <div className="alert-success">Delivered at {deliveredAt}</div>
+              ) : (
+                <div className="alert-error">Not delivered</div>
+              )}
+            </div>
+
+            <div className="card p-5">
+              <h2 className="mb-2 text-lg">Payment Method</h2>
+              {paymentMethod}
+              {isPaid ? (
+                <div className="alert-success">Paid at {paidAt}</div>
+              ) : (
+                <div className="alert-error">Not paid</div>
+              )}
+            </div>
+
+            <div className="card overflow-x-auto p-5">
+              <h2 className="mb-2 text-lg">Order Items</h2>
+              <table className="min-w-full">
+                <thead className="border-b">
+                  <tr>
+                    <th className="px-5 text-left">Item</th>
+                    <th className="p-5 text-right">Quantity</th>
+                    <th className="p-5 text-right">Price</th>
+                    <th className="p-5 text-right">Subtotal</th>
+                  </tr>
+                </thead>
+
+                <tbody>
+                  {orderItems.map((item) => (
+                    <tr
+                      key={item._id}
+                      className="border-b">
+                      <td>
+                        <Link href={`/product/${item.slug}`}>
+                          <a className="flex items-center">
+                            <Image
+                              src={item.image}
+                              alt={item.name}
+                              width={50}
+                              height={50}></Image>
+                            &nbsp;
+                            {item.name}
+                          </a>
+                        </Link>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
       )}
     </Layout>
   );
