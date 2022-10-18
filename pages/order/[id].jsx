c81177ad875de5424f9729a70961abc89fed55d4
Roman Trynko <trynkoroman25@gmail.com>
2022-10-18 13:36:11 +0300

    added and handled order history page

--- pages/order/[id].jsx
+++ pages/order/[id].jsx
@@ -9,29 +9,8 @@ import usePayment from '../../hooks/usePayment';
 import { toast } from 'react-toastify';
 import { PayPalButtons, usePayPalScriptReducer } from '@paypal/react-paypal-js';
 
-function reducer(state, action) {
-  switch (action.type) {
-    case 'FETCH_REQUEST':
-      return { ...state, loading: true, error: '' };
-    case 'FETCH_SUCCESS':
-      return { ...state, loading: false, order: action.payload, error: '' };
-    case 'FETCH_FAIL':
-      return { ...state, loading: false, error: action.payload };
-    case 'PAY_REQUEST':
-      return { ...state, loadingPay: true };
-    case 'PAY_SUCCESS':
-      return { ...state, loadingPay: false, successPay: true };
-    case 'PAY_FAIL':
-      return { ...state, loadingPay: false, errorPay: action.payload };
-    case 'PAY_RESET':
-      return { ...state, loadingPay: false, errorPay: '' };
-    default:
-      return state;
-  }
-}
-
 function OrderScreen() {
-  // const { reducer } = usePayment();
+  const { reducer } = usePayment();
 
   const { query } = useRouter();
   const orderId = query.id;
