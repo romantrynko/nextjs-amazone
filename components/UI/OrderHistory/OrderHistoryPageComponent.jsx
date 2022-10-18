e301a521ede3ea3e5e9cea214b4f617d86f5a732
Roman Trynko <trynkoroman25@gmail.com>
2022-10-18 17:33:37 +0300

    refactored order history component

---
+++ components/UI/OrderHistory/OrderHistoryPageComponent.jsx
@@ -0,0 +1,53 @@
+import Link from 'next/link';
+import React from 'react'
+import useOrderHistoryPage from '../../../hooks/useOrderHistoryPage';
+
+const OrderHistoryPageComponent = () => {
+  const { orders } = useOrderHistoryPage();
+
+  return (
+    <div className="overflow-x-auto">
+      <table className="min-w-full">
+        <thead className="border-b">
+          <tr>
+            <th className="px-5 text-left">ID</th>
+            <th className="p-5 text-left">TOTAL</th>
+            <th className="p-5 text-left">DATE</th>
+            <th className="p-5 text-left">PAID</th>
+            <th className="p-5 text-left">DELIVERED</th>
+            <th className="p-5 text-left">ACTION</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {orders.map((order) => (
+            <tr
+              key={order._id}
+              className="border-b">
+              <td className="p-5">{order._id.substring(20, 24)}</td>
+              <td className="p-5">${order.totalPrice}</td>
+              <td className="p-5">{order.createdAt.substring(0, 10)}</td>
+              <td className="p-5">
+                {order.isPaid ? `${order.paidAt.substring(0, 10)}` : 'not paid'}
+              </td>
+              <td className="p-5">
+                {order.isDelivered
+                  ? `${order.deliveredAt.substring(0, 10)}`
+                  : 'not delivered'}
+              </td>
+              <td className="p-5">
+                <Link
+                  href={`/order/${order._id}`}
+                  passHref>
+                  <a>Details</a>
+                </Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default OrderHistoryPageComponent
--- pages/order-history.jsx
+++ pages/order-history.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Layout from '../components/Layout';
-import Link from 'next/link';
 import useOrderHistoryPage from '../hooks/useOrderHistoryPage';
+import OrderHistoryPageComponent from '../components/UI/OrderHistory/OrderHistoryPageComponent';
 
 const OrderHistoryScreen = () => {
-  const { loading, error, orders } = useOrderHistoryPage();
+  const { loading, error } = useOrderHistoryPage();
 
   return (
     <Layout title="Order History">
@@ -14,49 +14,7 @@ const OrderHistoryScreen = () => {
       ) : error ? (
         <div className="alert-error">{error}</div>
       ) : (
-        <div className="overflow-x-auto">
-          <table className="min-w-full">
-            <thead className="border-b">
-              <tr>
-                <th className="px-5 text-left">ID</th>
-                <th className="p-5 text-left">TOTAL</th>
-                <th className="p-5 text-left">DATE</th>
-                <th className="p-5 text-left">PAID</th>
-                <th className="p-5 text-left">DELIVERED</th>
-                <th className="p-5 text-left">ACTION</th>
-              </tr>
-            </thead>
-
-            <tbody>
-              {orders.map((order) => (
-                <tr
-                  key={order._id}
-                  className="border-b">
-                  <td className="p-5">{order._id.substring(20, 24)}</td>
-                  <td className="p-5">${order.totalPrice}</td>
-                  <td className="p-5">{order.createdAt.substring(0, 10)}</td>
-                  <td className="p-5">
-                    {order.isPaid
-                      ? `${order.paidAt.substring(0, 10)}`
-                      : 'not paid'}
-                  </td>
-                  <td className="p-5">
-                    {order.isDelivered
-                      ? `${order.deliveredAt.substring(0, 10)}`
-                      : 'not delivered'}
-                  </td>
-                  <td className="p-5">
-                    <Link
-                      href={`/order/${order._id}`}
-                      passHref>
-                      <a>Details</a>
-                    </Link>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+       <OrderHistoryPageComponent/>
       )}
     </Layout>
   );
