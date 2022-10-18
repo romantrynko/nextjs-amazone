d8a58435aa75a58a25e9ef07f6b52e0500b2dbbb
Roman Trynko <trynkoroman25@gmail.com>
2022-10-18 17:29:21 +0300

    refactored login component

---
+++ components/UI/Login/LoginPageComponent.jsx
@@ -0,0 +1,63 @@
+import Link from 'next/link';
+import React from 'react'
+import useLoginPage from '../../../hooks/useLoginPage';
+
+const regEx = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/i
+
+const LoginPageComponent = () => {
+   const { handleSubmit, register, submitHandler, errors, redirect } =
+     useLoginPage();
+
+  return (
+    <form
+      className="mx-auto max-w-screen-md"
+      onSubmit={handleSubmit(submitHandler)}>
+      <h1 className="mb-4 text-xl">Login</h1>
+      <div className="mb-4">
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          {...register('email', {
+            required: 'Please enter email',
+            pattern: {
+              value: regEx,
+              message: 'Please enter valid email'
+            }
+          })}
+          id="email"
+          className="w-full"
+          autoFocus></input>
+        {errors.email && (
+          <div className="text-red-500">{errors.email.message}</div>
+        )}
+      </div>
+      <div className="mb-4">
+        <label htmlFor="password">Password</label>
+        <input
+          {...register('password', {
+            required: 'Please enter password',
+            minLength: {
+              value: 3,
+              message: 'Password is less than 3 characters'
+            }
+          })}
+          type="password"
+          id="password"
+          className="w-full"
+          autoFocus></input>
+        {errors.password && (
+          <div className="text-red-500">{errors.password.message}</div>
+        )}
+      </div>
+      <div className="mb-4">
+        <button className="primary-button">Login</button>
+      </div>
+      <div className="mb-4">
+        Don&apos;t have an account? &nbsp;
+        <Link href={`/register?redirect=${redirect || '/'}`}>Register</Link>
+      </div>
+    </form>
+  );
+}
+
+export default LoginPageComponent
--- pages/login.jsx
+++ pages/login.jsx
@@ -1,62 +1,11 @@
 import Layout from '../components/Layout';
-import Link from 'next/link';
 import React from 'react';
-import useLoginPage from '../hooks/useLoginPage';
+import LoginPageComponent from '../components/UI/Login/LoginPageComponent';
 
 const LoginScreen = () => {
-  const { handleSubmit, register, submitHandler, errors, redirect } =
-    useLoginPage();
-
   return (
     <Layout title="login">
-      <form
-        className="mx-auto max-w-screen-md"
-        onSubmit={handleSubmit(submitHandler)}>
-        <h1 className="mb-4 text-xl">Login</h1>
-        <div className="mb-4">
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            {...register('email', {
-              required: 'Please enter email',
-              pattern: {
-                value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/i,
-                message: 'Please enter valid email'
-              }
-            })}
-            id="email"
-            className="w-full"
-            autoFocus></input>
-          {errors.email && (
-            <div className="text-red-500">{errors.email.message}</div>
-          )}
-        </div>
-        <div className="mb-4">
-          <label htmlFor="password">Password</label>
-          <input
-            {...register('password', {
-              required: 'Please enter password',
-              minLength: {
-                value: 3,
-                message: 'Password is less than 3 characters'
-              }
-            })}
-            type="password"
-            id="password"
-            className="w-full"
-            autoFocus></input>
-          {errors.password && (
-            <div className="text-red-500">{errors.password.message}</div>
-          )}
-        </div>
-        <div className="mb-4">
-          <button className="primary-button">Login</button>
-        </div>
-        <div className="mb-4">
-          Don&apos;t have an account? &nbsp;
-          <Link href={`/register?redirect=${redirect || '/'}`}>Register</Link>
-        </div>
-      </form>
+      <LoginPageComponent/>
     </Layout>
   );
 };
